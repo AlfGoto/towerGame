6fd75febdc9e8767af2b8dd25c587892eb78df15
AlfGoto <147511905+AlfGoto@users.noreply.github.com>
2024-07-09 09:50:27 +0200

    pause system done

--- classes/tools/debug.js
+++ classes/tools/debug.js
@@ -3,9 +3,19 @@ import x2 from '../options/x2.js'
 
 export default class {
     constructor() {
-        document.addEventListener('keypress', e => {
+        this.pause = false
+        document.addEventListener('keydown', e => {
+            // console.log(e)
             if (e.key === "k") { towerStats.dealDamage(5) }
             if (e.key === " ") { if (x2.speed == 1) { x2.speed = 2 } else x2.speed = 1 }
+            if (e.key === "Escape") {
+                if (this.pause) {
+                    towerStats.gameOn = true
+                } else {
+                    towerStats.gameOn = false
+                }
+                this.pause = !this.pause
+            }
         })
     }
 }
